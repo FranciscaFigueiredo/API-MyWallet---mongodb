04a53599a607217f57a4943549a877a2edd9a1d7
Francisca <fraan928@gmail.com>
2022-02-02 02:00:24 -0300

    Route refactoring

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import { signUp } from './controllers/userController.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/sign-up', signUp);
+app.use('', userRouter);
 
 export {
     app,
