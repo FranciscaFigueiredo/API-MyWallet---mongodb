57c770114b37b0a2ea8b6c11823fa6163e96574c
Francisca <fraan928@gmail.com>
2022-02-02 02:35:14 -0300

    Authentication middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,20 @@
-function auth(req, res, next) {
+import { connection } from '../database.js';
+
+async function auth(req, res, next) {
     const token = req.headers.authorization?.replace('Bearer ', '');
 
     if (!token) {
         return res.sendStatus(401);
     }
+
+    const db = await connection({ column: 'sessions' });
+
+    const userSession = await db.findOne({ token });
+
+    const userId = userSession._id;
+
+    res.locals.user = userId;
+
     return next();
 }
 
